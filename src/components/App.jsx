249fb1f1c96df3bc212cd340c187b6097f40eb20
Oleksandr Kokhno <kohnoan@gmail.com>
2023-05-07 22:30:20 +0300

    commit 6.05.23

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import React from 'react';
+import { Statistics } from './Statistics/Statistics';
+
+class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, bad, neutral } = this.state;
+    const total = this.countTotalFeedback();
+    return (
+      <>
+        {/* <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={Object.keys(this.state)}
+            onLeaveFeedback={this.handleLeaveFeedback}
+          />
+        </Section> */}
+        {/* {total === 0 ? (
+          <Notification message="There is no feedback" />
+        ) : (
+          <Section title="Statistics"> */}
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={total}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          {/* </Section> */}
+        {/* )} */}
+      </>
+    );
+        }
+}
+
+
+
+
+
+  export { App };
+
+
+// export const App = () => {
+//   return (
+//     <div
+//       style={{
+//         height: '100vh',
+//         display: 'flex',
+//         justifyContent: 'center',
+//         alignItems: 'center',
+//         fontSize: 40,
+//         color: '#010101'
+//       }}
+//     >
+//       React homework template
+//     </div>
+//   );
+// };
